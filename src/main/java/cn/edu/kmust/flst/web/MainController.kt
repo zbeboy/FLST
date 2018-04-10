49f3a27e6e0069fa6ccc7e880312c28e47c0efe5
zbeboy <863052317@qq.com>
2018-04-10 20:45:49 +0800

    登录页面处理

--- src/main/java/cn/edu/kmust/flst/web/MainController.kt
+++ src/main/java/cn/edu/kmust/flst/web/MainController.kt
@@ -4,6 +4,7 @@ import cn.edu.kmust.flst.config.Workbook
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import javax.servlet.http.HttpServletRequest
 
 @Controller
@@ -31,6 +32,16 @@ open class MainController {
         val path = request.session.servletContext.getRealPath(Workbook.DIRECTORY_SPLIT)
         modelMap.addAttribute("path",path)
         return "index"
+    }
+
+    /**
+     * 登录页
+     *
+     * @return 登录页.
+     */
+    @RequestMapping(value = ["/login"], method = [(RequestMethod.GET)])
+    fun login(): String {
+        return "login"
     }
 
 }
