ebb9e58434257721c2be667d961eb47363579657
zbeboy <863052317@qq.com>
2018-04-15 13:59:17 +0800

    添加友情链接以及网站信息

--- src/main/java/cn/edu/kmust/flst/config/Workbook.kt
+++ src/main/java/cn/edu/kmust/flst/config/Workbook.kt
@@ -57,6 +57,7 @@ open class Workbook {
         const val WEBSITE_ZIP_CODE = "zipCode"
         const val WEBSITE_PHONE = "phone"
         const val WEBSITE_FAX = "fax"
+        const val WEBSITE_ADDRESS_EN = "address_en"
 
         /**
          * 自定义图片路径
